0d2052fe530d8f408353ba1a606f06a08e069411
eshejivoi <kira.nikulina777@gmail.com>
2024-11-10 12:35:16 +0300

    dhjhdj

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from translate import Translator
+
+# Создаем переводчика с русского на английский
+translator = Translator(from_lang="ru", to_lang="en")
+
+# Переводим текст
+translation = translator.translate("Привет, как дела?")
+print(translation)  # Hello, how are you?
+
+
